4f13f0ad3025f66935178002edd97cc52c42048c
HayrtonFerreira <josehayrtonferreira@gmail.com>
2023-09-07 23:45:55 -0300

    corecao

--- src/ex02.java
+++ src/ex02.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class ex02 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double valor1, valor2, resultado;
+        
+        System.out.println("Digite o primeiro valor: ");
+        valor1 = scanner.nextDouble();
+        
+        System.out.println("Digite o segundo valor (não pode ser zero): ");
+        valor2 = scanner.nextDouble();
+        
+        while (valor2 == 0) {
+            System.out.println("Valor inválido! Digite novamente: ");
+            valor2 = scanner.nextDouble();
+        }
+        
+        resultado = valor1 / valor2;
+        System.out.println("O resultado da divisão é: " + resultado);
+        
+        scanner.close();
+    }
 }
+
--- src/ex04.java
+++ src/ex04.java
@@ -1,3 +1,24 @@
-public class ex04 {
-    
+import java.util.Scanner;
+
+    public class ex04 { 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe o primeiro valor: ");
+        double primeiroValor = scanner.nextDouble();
+
+        System.out.print("Informe o segundo valor: ");
+        double segundoValor = scanner.nextDouble();
+
+        while (segundoValor == 0) {
+            System.out.println("O segundo valor não pode ser zero.");
+            System.out.print("Informe um novo valor: ");
+            segundoValor = scanner.nextDouble();
+        }
+
+        double resultado = primeiroValor / segundoValor;
+        System.out.println("O resultado da divisão é: " + resultado);
+
+        scanner.close();
+    }
 }
